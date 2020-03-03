d05f5ffe8849cdf446677dd50602215c32ba7391
Daniel <jose.marinmontero@ucr.ac.cr>
2020-03-02 20:46:51 -0600

    Agrega archivos parar crear y poblar la base de datos

---
+++ PostgreSQL/DMLaeropuertos.sql
@@ -0,0 +1,4 @@
+-- Código, Nombre, Dirección,Habilitado
+INSERT INTO aeropuerto VALUES('SJO','Juan Santamaría','Provincia de Alajuela, Alajuela',TRUE);
+INSERT INTO aeropuerto VALUES('LIR','Daniel Oduber','Boulevard Aeropuerto S/N, Provincia de Guanacaste, Liberia',TRUE);
+INSERT INTO aeropuerto VALUES('SYQ','Tobías Bolaños','Aeropuerto, San José',TRUE);
